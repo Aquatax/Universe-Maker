edc6e6650ed7d8ae65160f05470816c90772cff6
Brandon <dinospaceh2o@gmail.com>
2023-04-29 22:21:48 -0400

    0.1.2
    COMMANDS!
    
    -Renamed main.py to galaxymaker.py
    -main.py is now the home to the commands page
    Running main.py will allow you to use a few initial commands, these are:
    /make planet ... : This will make a planet with the variables provided
    /make galaxy ... : This will make a galaxy with the variables provided
    /make moon ...   : This will make a moon with the variables provided
    /make help       : This shows the help page for /make. This is also showed with an invalid command
    
    /help: This shows a basic guide on available commands
    
    -Moons can now be force created as a certain moon type (moon1, moon2, moon3, moon4, moon5)

---
+++ galaxymaker.py
@@ -0,0 +1,119 @@
+from random import randint
+import planetmaker
+
+
+def galaxymakerr(minimum_systems, include_moons=True):
+    print("HERE")
+    # Code by Aquatax
+    # Write Below Here \/ \/ \/
+
+    number_of_systems = round(minimum_systems)
+    number_of_overflow_systems = round(minimum_systems / 2)
+    moon_enabled = True
+
+    # Write above here /\ /\ /\
+
+    lifecounter = 0
+
+    def system_checker(systemb, systemc):
+        if systemb == systemc:
+            return True
+        else:
+            return False
+
+    movements = 0
+
+    systems = {}
+    for x in range(1, number_of_systems + 1 + number_of_overflow_systems):
+        newSystem = str(f"system{x}")
+        systems[newSystem] = [{
+                                  "links": []}, {
+                                  "planets": []}]
+
+    systems_broken = 0
+    systems["system1"][0]["links"] = [2, 3, 4, 5, 6, 7, 8, 9]
+
+    # print(systems["system1"][0]["links"])
+    for x in systems["system1"][0]["links"]:
+        selectSystem = f"system{x}"
+        systems[selectSystem][0]["links"].append(1)
+
+    # print(systems)
+    for x in range(2, number_of_systems + 1):
+
+        SysteminQuestion = f"system{x}"
+        startingLength = len(systems[SysteminQuestion][0]["links"])
+        if startingLength < 8:
+            while len(systems[SysteminQuestion][0]["links"]) < 8:
+                systema = randint(2, number_of_systems + number_of_overflow_systems)
+                while system_checker(systema, x) == True:
+                    systema = randint(2, number_of_systems + number_of_overflow_systems)
+                newsysteminquestion = f"system{systema}"
+                if len(systems[newsysteminquestion][0]["links"]) < 8:
+                    go = 1
+
+                    # checks if the number is repeated in the list. If it does repeat in the list it will move on
+                    # and not append the number
+                    for z in systems[SysteminQuestion][0]["links"]:
+                        if z == systema:
+                            go = 0
+                            systems_broken += 1
+                            if SysteminQuestion == number_of_systems:
+                                go = 1
+
+                            if systems_broken > 3:
+                                go = 2
+                                systems_broken = 0
+
+                    if go == 1:
+                        movements += 1
+                        systems[newsysteminquestion][0]["links"].append(x)
+                        systems[SysteminQuestion][0]["links"].append(systema)
+                elif x == newsysteminquestion:
+                    pass
+
+    for x in range(2, number_of_systems + 1):
+
+        SysteminQuestion = f"system{x}"
+        number_of_planets = randint(0, 10)
+        # At most 9 planets. If planet number is 0, it should skip
+        for planetnum in range(0, number_of_planets):
+            newplanet = planetmaker.makeplanet()
+            planetinfo = newplanet.planetfactory(include_moons=moon_enabled)
+            #                 Example of what would be returned
+            # return [{"Type": typeer}, {"Gravity": gravity}, {"Ringed": hasring}, {"Subtype": subtype},
+            # {"Has Life": haslife}, {"Life Subtypes": lifesubtypes}, {"Settled": settled}]
+
+            # Ideal planet dict
+            # "Planets": [{"Planet1": [{"Name": "Jupiter"}, {"Type": "Gas Giant"}, {"Ringed: True"}, {"Subtype": "Gaseous"},
+            #  {"Has Life": False}, {"Life Subtypes": None}{"Settled": False}, {"Gravity": 5}, ]}]
+
+            typeer = planetinfo["Type"]
+            grav = planetinfo["Gravity"]
+            ringed = planetinfo["Ringed"]
+            subtype = planetinfo["Subtype"]
+            haslife = planetinfo["Has Life"]
+            lifesubtype = planetinfo["Life Subtypes"]
+            settled = planetinfo["Settled"]
+
+            moons = planetinfo["Moons"]
+            planetpoint = f"planet{planetnum}"
+            if settled:
+                lifecounter += 1
+
+            newplaneta = {
+                planetpoint: [{
+                                  "Name": planetpoint}, {
+                                  "Type": typeer}, {
+                                  "Ringed": ringed},
+                              {
+                                  "Subtype": subtype}, {
+                                  "Has Life": haslife}, {
+                                  "Life Subtypes": lifesubtype},
+                              {
+                                  "Settled": settled}, {
+                                  "Gravity": grav}, {
+                                  "Moons": moons}]}
+            systems[SysteminQuestion][1]["planets"].append(newplaneta)
+
+    return systems
